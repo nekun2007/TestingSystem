018caea97d43afefa79e0cb26864e8fa616819f5
Nikki <nekun2007@gmail.com>
2017-03-25 16:17:18 +0300

    Добавили отображение панелей

--- src/main/java/StudentMainFrame.java
+++ src/main/java/StudentMainFrame.java
@@ -6,26 +6,42 @@ import java.awt.*;
  */
 public class StudentMainFrame {
     JFrame studentFrame;
-    JPanel[] panels;
     JRadioButton[] jRadioButtons;
     JLabel[] jLabels;
+    ButtonGroup[] groups;
     public StudentMainFrame() {
         studentFrame = new JFrame(Const.PROGRAM_NAME);
-        Container container = new Container();
-        JScrollPane jScrollPane = new JScrollPane(container);
-        panels = new JPanel[20];
+       JPanel container = new JPanel();
+        container.setLayout(new BoxLayout(container,BoxLayout.Y_AXIS));
+        JScrollPane[] jScrollPane = new JScrollPane[20];
+        JPanel[] panels = new JPanel[20];
+        JLabel[] labels = new JLabel[20];
+        groups = new ButtonGroup[80];
+        JScrollPane jScrollPane1 = new JScrollPane(container,JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
         for(int i=0; i<20; i++) {
             panels[i] = new JPanel();
-            panels[i].add(new JLabel("Hello #" + i ));
             panels[i].setVisible(true);
-            studentFrame.add(panels[i],i);
+            panels[i].add(new JLabel("Hello #" + i));
+            groups[i] = new ButtonGroup();
+
+            groups[i].add(new JRadioButton("test1"));
+            groups[i].add(new JRadioButton("test1"));
+            groups[i].add(new JRadioButton("test1"));
+            groups[i].add(new JRadioButton("test1"));
+            panels[i].add();
+            container.add(panels[i]);
         }
 
-      // studentFrame.setLayout(new BoxLayout(studentFrame,BoxLayout.Y_AXIS));
+        //TODO: добавить кнопку "Завершить"
 
 
+        studentFrame.getContentPane().add(jScrollPane1);
         studentFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         studentFrame.setSize(800,800);
         studentFrame.setVisible(true);
+    }
+
+    private void generateQuetion() {
+        //TODO: генерация списка вопросов
     }
 }
